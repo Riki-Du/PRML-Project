9dc81acbf2f0f4eb694b38c2fc60c5de40223472
Riki Du <duyixian19990123@hotmail.com>
2020-07-07 12:17:27 +0800

    修改example

--- utils.py
+++ utils.py
@@ -1,4 +1,128 @@
-import read_pysmiles
-import read_rdkit
+import os
+import sys
+import csv
+import dgl
+import numpy as np
+import random
+import torch
 
-read_pysmiles.read_from_pysmiles(0)
+from dgllife.utils import smiles_to_complete_graph
+
+# get a path
+def GetPath(file):
+    path = sys.path[0]
+    path = os.path.normpath(path)
+    return os.path.join(path, file)
+
+# open a csv
+def OpenCSV(file):
+    file = os.path.normpath(file)
+    file = GetPath(file)
+    f = open(file)
+    f_csv = csv.reader(f)
+    return f_csv
+
+# get train / test path everytime
+def train_test_path(num=10):
+    train_path = ""
+    test_path = ""
+    dev_path = ""
+    if num == 10:
+        train_path = "data/train.csv"
+        test_path = "data/test.csv"
+    else:
+    # walk through fold 0-9
+        train_path = "data/train_cv/fold_" + str(num) + "/train.csv"
+        test_path = "data/train_cv/fold_" + str(num) + "/test.csv"
+        dev_path = "data/train_cv/fold_" + str(num) + "/dev.csv"
+
+    return train_path, test_path , dev_path
+
+def read_from_rdkit(num, choice):
+    train_path, test_path, dev_path = train_test_path(num)
+    path = train_path
+    if choice == 1:
+        path = test_path
+    elif choice == 2:
+        path = dev_path
+    f_csv = OpenCSV(path)
+    SMILES_list = []
+    if num == 10:
+        # id,smiles,activity
+        if choice == 0:
+            SMILES_list = [(row[1],row[2]) for row in f_csv]
+        else:
+            SMILES_list = [(row[1]) for row in f_csv]
+    else:
+        # smiles,activity
+        SMILES_list = [(row[0],row[1]) for row in f_csv]
+    # SMILES = SMILES_list[1]
+    # print("Test for rdkit!")
+    # print(SMILES)
+    SMILES_list = SMILES_list[1:]
+    sm = [row[0] for row in SMILES_list]
+    labels = [row[1] for row in SMILES_list]
+    return sm, labels
+
+def load_data(num): 
+    trainmols, train_y = read_from_rdkit(num,0)
+    testmols, test_y = read_from_rdkit(num,1)
+    train_g = [smiles_to_complete_graph(m, add_self_loop=False) for m in trainmols]
+    train_y = np.array(train_y, dtype=np.int64)
+    print("Training set ",len(train_g))
+    
+    test_g = [smiles_to_complete_graph(m, add_self_loop=False) for m in testmols]
+    test_y = np.array(test_y, dtype=np.int64)
+    print("Test set",len(test_g))
+    print("Data loaded.")
+
+    return train_g, train_y, test_g, test_y
+
+
+def set_random_seed(seed=0):
+    """Set random seed.
+
+    Parameters
+    ----------
+    seed : int
+        Random seed to use. Default to 0.
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+
+def collate_molgraphs(data):
+    graphs, labels = map(list,zip(*data))
+
+    batched_graph = dgl.batch(graphs)
+    batched_graph.set_n_initializer(dgl.init.zero_initializer)
+    batched_graph.set_e_initializer(dgl.init.zero_initializer)
+
+    return batched_graph, torch.tensor(labels)
+
+def load_model(args):
+    if args['model'] == 'SchNet':
+        from dgllife.model import SchNetPredictor
+        model = SchNetPredictor(node_feats=args['node_feats'],
+                                hidden_feats=args['hidden_feats'],
+                                classifier_hidden_feats=args['classifier_hidden_feats'],
+                                n_tasks=args['n_tasks'])
+
+    if args['model'] == 'MGCN':
+        from dgllife.model import MGCNPredictor
+        model = MGCNPredictor(feats=args['feats'],
+                              n_layers=args['n_layers'],
+                              classifier_hidden_feats=args['classifier_hidden_feats'],
+                              n_tasks=args['n_tasks'])
+
+    if args['model'] == 'MPNN':
+        from dgllife.model import MPNNPredictor
+        model = MPNNPredictor(node_in_feats=args['node_in_feats'],
+                              edge_in_feats=args['edge_in_feats'],
+                              node_out_feats=args['node_out_feats'],
+                              edge_hidden_feats=args['edge_hidden_feats'],
+                              n_tasks=args['n_tasks'])
+
+    return model
